7c8348d2eb9867108561b893212530204bc2d845
Escarpe <rudenko.max1@gmail.com>
2018-07-03 00:57:06 +0300

    add Header styling

--- src/components/hoc/Header/header.js
+++ src/components/hoc/Header/header.js
@@ -8,30 +8,48 @@ import { searching, home, tweet, direct } from './strings';
 const Home = () => {
     return(
         <div className="header">
-            <div className="container">
-                <div className="navigation">
+            <div className="container" align="center">
+                <div className="nav-left">
                     <ul>
                         <li>
-                            <Link to="/home">{home}</Link>
+                            <Link to="/home" className="nav-left__link">
+                                <Icons 
+                                    icon='home' 
+                                    size="15px" 
+                                    color="#a8a8a8"
+                                    style={{margin: '0 5px 0 0'}}
+                                />
+                                <span>{home}</span>
+                            </Link>
                         </li>
                         <li>
-                            <Link to="/direct">{direct}</Link>
+                            <Link to="/direct" className="nav-left__link">
+                                <Icons 
+                                    icon='mail' 
+                                    size="15px" 
+                                    color="#a8a8a8"
+                                    style={{margin: '0 5px 0 0'}}
+                                />
+                                <span>{direct}</span>
+                            </Link>
                         </li>
                     </ul>
                 </div>
                 <div className="logo">
+                    <Link to="/home"><Icons icon='twitter' size="20px" color="#1da1f2"/></Link>
                 </div>
                 <div className="nav-right">
                     <div className="search">
-                        <form className="search-form">
-                            <input type="text" placeholder="{search}" className="search-input" ></input>
+                        <form className="search__form" >
+                            <input type="text" placeholder={searching} className="search-input" ></input>
                         </form>
                     </div>
                     <button className="tweet-button">
-                        <span>{tweet}</span>
+                        <span><b>{tweet}</b></span>
                     </button>
                 </div>
              </div>
+             <hr />
         </div>
     )
 }
