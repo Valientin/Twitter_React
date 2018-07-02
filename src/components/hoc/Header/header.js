32f5a21acc0be8b82553f0f98e693900df93007b
Escarpe <rudenko.max1@gmail.com>
2018-07-03 01:32:43 +0300

    change header styling

--- src/components/hoc/Header/header.js
+++ src/components/hoc/Header/header.js
@@ -5,9 +5,13 @@ import './header.scss';
 import Icons from '../../Icons';
 import { searching, home, tweet, direct } from './strings';
 
-const Home = () => {
-    return(
-        <div className="header">
+
+
+ export default class Header extends React.Component{
+
+    showTemplate = (user) => {
+        let template;
+         user? template = (<div className="header">
             <div className="container" align="center">
                 <div className="nav-left">
                     <ul>
@@ -30,28 +34,53 @@ const Home = () => {
                                     color="#a8a8a8"
                                     style={{margin: '0 5px 0 0'}}
                                 />
-                                <span>{direct}</span>
+                                   <span>{direct}</span>
                             </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="logo">
+                           </li>
+                        </ul>
+                  </div>
+                 <div className="logo">
                     <Link to="/home"><Icons icon='twitter' size="20px" color="#1da1f2"/></Link>
-                </div>
-                <div className="nav-right">
-                    <div className="search">
-                        <form className="search__form" >
-                            <input type="text" placeholder={searching} className="search-input" ></input>
-                        </form>
-                    </div>
-                    <button className="tweet-button">
-                        <span><b>{tweet}</b></span>
-                    </button>
+                 </div>
+                  <div className="nav-right">
+                       <div className="search">
+                         <form className="search__form" >
+                             <input type="text" placeholder={searching} className="search-input" ></input>
+                         </form>
+                     </div>
+                     <button className="tweet-button">
+                         <span><b>{tweet}</b></span>
+                      </button>
+                     </div>
+                 </div>
+                 <hr />
+             </div>)
+        : template = (<div className="header">
+            <div className="container" >
+                <div className="nav-left" >
+                    <Link to="/home" className="nav-left__link" >
+                        <Icons 
+                            icon='twitter' 
+                            size="20px" 
+                            color="#1da1f2"
+                            style={{margin: '0 5px 0 0'}}
+                        />
+                        <span>{home}</span>
+                     </Link>
                 </div>
              </div>
              <hr />
-        </div>
-    )
+        </div>)
+
+return template
 }
 
-export default Home;
+
+    render(){
+        return(
+        <header>
+            {this.showTemplate()}
+        </header>
+    )
+    }
+}
