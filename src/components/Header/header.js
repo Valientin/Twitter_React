cc077383072ea0006fed22e1fa1feb178e48b215
Escarpe <rudenko.max1@gmail.com>
2018-07-03 15:04:00 +0300

    add user route

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -73,7 +73,7 @@ export default class Header extends React.Component{
     render(){
         return(
             <header>
-                {this.showTemplate(false)}
+                {this.showTemplate(true)}
             </header>
         )
     }
