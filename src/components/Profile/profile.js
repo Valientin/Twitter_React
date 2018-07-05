6864f308267d25de19c2743ee063fbf30aadb51c
Valientine <30551708+Valientin@users.noreply.github.com>
2018-07-05 12:26:55 +0300

    Add change function

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -2,20 +2,117 @@ import React from 'react';
 import { NavLink, Switch } from 'react-router-dom';
 import moment from 'moment';
 import { Redirect } from 'react-router'
+
 import PrivateRoutes from '../hoc/AuthRoute/privateRoute';
+import FormField from '../FormFields';
+import Icons from '../Icons';
 import Tweets from '../Tweets';
 import Followers from '../Followers';
 import Followed from '../Followed';
 
+import { validate } from '../utils';
 import './profile.scss';
-import Icons from '../Icons';
+
 
 class Profile extends React.Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			changeProfile: false
+			changeProfile: false,
+			userData: {
+				name: 'faf',
+				lastname: '',
+				userName: '',
+				about: '',
+				city: '',
+				internet: '',
+				dete: ''
+			},
+			formData: {
+				name: {
+					element: 'input',
+					value: '',
+					config: {
+						name: 'name_input',
+						type: 'text',
+						className: 'input__change input-change__name',
+						placeholder: 'Имя'
+					},
+					validation: {
+						required: true,
+						name: true
+					},
+					valid: false,
+					touched: false,
+					validationMessage: ''
+				},
+				about: {
+					element: 'input',
+					value: '',
+					config: {
+						name: 'about_input',
+						type: 'text',
+						className: 'input__change input-change__about',
+						placeholder: 'О себе'
+					},
+					validation: {
+						required: false
+					},
+					valid: false,
+					touched: false,
+					validationMessage: ''
+				},
+				city: {
+					element: 'input',
+					value: '',
+					config: {
+						name: 'city_input',
+						type: 'text',
+						className: 'input__change input-change__city',
+						placeholder: 'Местоположение'
+					},
+					validation: {
+						required: false
+					},
+					valid: false,
+					touched: false,
+					validationMessage: ''
+				},
+				internet: {
+					element: 'input',
+					value: '',
+					config: {
+						name: 'internet_input',
+						type: 'text',
+						className: 'input__change input-change__internet',
+						placeholder: 'Ваш сайт'
+					},
+					validation: {
+						required: false
+					},
+					valid: false,
+					touched: false,
+					validationMessage: ''
+				},
+				date: {
+					element: 'input',
+					value: '',
+					config: {
+						name: 'date_input',
+						type: 'text',
+						className: 'input__change input-change__date',
+						placeholder: 'Дата рождения (DD/MM/YYYY)'
+					},
+					validation: {
+						required: true,
+						date: true
+					},
+					valid: false,
+					touched: false,
+					validationMessage: ''
+				}
+			}
 		}
 	}
 	
@@ -23,6 +120,70 @@ class Profile extends React.Component {
 	componentDidMount(){
 		this.props.getProfileData(this.props.user.uid);
 	}
+
+	componentWillReceiveProps(nextProps) {
+		console.log(nextProps.profileData)
+		for(let key in nextProps.profileData){
+			if(this.state.formData[key]){
+				const newData = nextProps.profileData[key];
+				this.updateForm({id: key, value: newData});
+			}
+		}
+		
+	}
+	
+
+	updateForm = (elem) => {
+        const newFormData = {
+            ...this.state.formData
+        }
+        const newElem = {
+            ...newFormData[elem.id]
+        }
+        elem.e ? newElem.value = elem.e.target.value: newElem.value = elem.value;
+        if(elem.blur){
+            let validData = validate(newElem);
+            newElem.valid = validData[0];
+            newElem.validationMessage = validData[1]
+        }
+
+        newElem.touched = elem.blur;
+        newFormData[elem.id] = newElem;
+
+        this.setState({
+            formData: newFormData
+        })
+    }
+
+
+    submitForm = (e, type) => {
+        e.preventDefault();
+        if(type !== null){
+            let dataToSubmit = {};
+            let formIsValid = true;
+
+            for(let key in this.state.formData){
+                dataToSubmit[key] = this.state.formData[key].value
+            }
+            for(let key in this.state.formData){
+                formIsValid = this.state.formData[key].valid && formIsValid;
+            }
+
+            if(formIsValid){
+                this.setState({
+                loading: true,
+                registerError: ''
+            })
+
+                console.log(dataToSubmit);
+
+            } else {
+                this.setState({
+                    registerError: validForm
+                })
+            }
+        }
+    }
 
 	toggleChangeProfile = () => {
 		this.setState({
@@ -33,10 +194,13 @@ class Profile extends React.Component {
 
 	checkData(option, number = false){
 		const profileData = this.props.profileData;
-		if(number) {
-			return profileData[option] ? profileData[option] : '0'
+		if(profileData[option]) {
+			
+			return profileData[option] ;
+		} else {
+			if(number) {return '0'};
+			return '';
 		}
-		return profileData[option] ? profileData[option] : ''
 	}
 
 	showChangeWrapper = () => (
@@ -76,9 +240,34 @@ class Profile extends React.Component {
 			</div>
 		: 
 			<div className="user-info change">
-					<h3>{`${this.checkData('name')} ${this.checkData('lastname')}`}</h3>
-					<span className="user-info__username">{`@${this.checkData('userName')}`}</span>
-					<div className="user-info__image"></div>
+				<FormField
+					id={'name'}
+					formData={this.state.formData.name}
+					change={(elem) => this.updateForm(elem)}
+				/>
+				<FormField
+					id={'about'}
+					formData={this.state.formData.about}
+					change={(elem) => this.updateForm(elem)}
+				/>
+				<FormField
+					id={'city'}
+					formData={this.state.formData.city}
+					change={(elem) => this.updateForm(elem)}
+				/>
+				<FormField
+					id={'internet'}
+					formData={this.state.formData.internet}
+					change={(elem) => this.updateForm(elem)}
+				/>
+				<div className="user-change__color">
+					<button className="user-change__color-button">Цвет темы</button>
+				</div>
+				<FormField
+					id={'date'}
+					formData={this.state.formData.date}
+					change={(elem) => this.updateForm(elem)}
+				/>
 			</div>
 
 	)
